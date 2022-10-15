929c2d07a04c528054dc743039541d5cd69fc422
afadili <ahmedfadilifd@gmail.com>
2022-10-15 10:59:29 +0200

    Anadida despedida

--- src/main/java/afad/imprimirHola.java
+++ src/main/java/afad/imprimirHola.java
@@ -11,5 +11,6 @@ public class imprimirHola
         System.out.println( "Hello World!" );
         System.out.println("How is your day going?");
 		System.out.println("Come back soon!!!");
+		System.out.println("Have a nice day and be happy!");
     }
 }
