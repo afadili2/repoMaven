8f87e97009ece8c4c5291c477dedc03a999d6d1e
afadili <ahmedfadilifd@gmail.com>
2022-10-15 10:03:10 +0200

    Primer commit de maven2

---
+++ src/main/java/afad/imprimirHola.java
@@ -0,0 +1,15 @@
+package afad;
+
+/**
+ * Hello world!
+ *
+ */
+public class imprimirHola 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("How is your day going?");
+		System.out.println("Come back soon!!!");
+    }
+}
---
+++ src/test/java/afad/ImprmirHolaTest.java
@@ -0,0 +1,20 @@
+package afad;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ImprmirHolaTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
